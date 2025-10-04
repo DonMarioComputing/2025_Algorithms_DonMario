18ee84a0edcff37ec64d7e12a9e72b14e5bd77bb
DonMarioComputing <omeshmariodon@gmail.com>
2025-10-04 14:08:44 +0100

    "feat: add execption for a null array"

--- src/main/java/utils/NumberUtils.java
+++ src/main/java/utils/NumberUtils.java
@@ -9,15 +9,19 @@ public class NumberUtils {
 
     /**
      * finds the first position of a given integer value in an array
-     * @param arr   search array
-     * @param value find value
+     *
+     * @param arr search array
+     * @param value find integer value
      * @return index of first occurrence of value, if not found -1 will pass
+     * @throws NullPointerException if the supplied array is null
      */
     public static int getPos(int[] arr, int value) {
+        if (arr == null) {
+            throw new NullPointerException("Array cannot be null");
+        }
         for (int i = 0; i < arr.length; i++) {
             if (arr[i] == value) return i;
         }
         return -1;
     }
 }
-

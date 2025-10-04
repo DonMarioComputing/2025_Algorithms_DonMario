c26fce5a9258e765793409875e782ce51d931953
DonMarioComputing <omeshmariodon@gmail.com>
2025-10-04 19:19:11 +0100

    "feat: Find the largest value in the random array using the NumberUtils class getMax() method"

--- src/main/java/ui/Statistics.java
+++ src/main/java/ui/Statistics.java
@@ -2,6 +2,7 @@ package ui;
 
 import utils.DataGenerator;
 import java.util.Arrays;
+import utils.NumberUtils;
 
 public class Statistics {
     public static void main(String[] args) {
@@ -17,6 +18,15 @@ public class Statistics {
         System.out.println("\nRandomly generated array contents: ");
         if (randomArray != null) {
             System.out.println(Arrays.toString(randomArray));
+
+        // use NumberUtils.getMax() to find and display the largest value in random array
+            try {
+                int largest = NumberUtils.getMax(randomArray);
+                System.out.println("\nLargest value in the random array: " + largest);
+            } catch (Exception e) {
+                System.out.println("Error finding largest value: " + e.getMessage());
+            }
+
         } else {
             System.out.println("Random array cannot be generated");
 

28103a44367c7a099c016ce1b57a25a2e21d532d
DonMarioComputing <omeshmariodon@gmail.com>
2025-10-04 14:30:41 +0100

    "feat: add getLastPos() method to find last position of an integer in an array with validation" "docs: add JavaDoc for getLastPos() method in NumberUtils"

--- src/main/java/utils/NumberUtils.java
+++ src/main/java/utils/NumberUtils.java
@@ -8,9 +8,9 @@ public class NumberUtils {
     /* Exercise 1 */
 
     /**
-     * finds the first position of a given integer value in an array
+     * finding the first position of a given integer value in an array
      *
-     * @param arr search array
+     * @param arr the search array
      * @param value find integer value
      * @return index of first occurrence of value, if not found -1 will pass
      * @throws NullPointerException if the supplied array is null
@@ -24,4 +24,27 @@ public class NumberUtils {
         }
         return -1;
     }
+
+    /* Exercise 2 */
+    /**
+     * finding the last position on the given integer value in array
+     *
+     * @param arr the search array
+     * @param value find integer value
+     * @return index of last occurrence of value, if not found -1 will pass
+     * @throws NullPointerException if the supplied array is null
+     */
+    public static int getLastPos(int[] arr, int value) {
+        if (arr == null) {
+            throw new NullPointerException("Array cannot be null");
+        }
+        for (int i = arr.length - 1; i >= 0; i--) {
+            if (arr[i] == value) return i;
+        }
+        return -1;
+    }
+
+
+
+
 }

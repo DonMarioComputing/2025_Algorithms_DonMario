301807fed94d53f7c32c3f72a379d46bb7fb09b5
DonMarioComputing <omeshmariodon@gmail.com>
2025-10-04 18:48:44 +0100

    "feat: Display the contents of both arrays. "

--- src/main/java/ui/Statistics.java
+++ src/main/java/ui/Statistics.java
@@ -1,4 +1,26 @@
 package ui;
 
+import utils.DataGenerator;
+import java.util.Arrays;
+
 public class Statistics {
+    public static void main(String[] args) {
+        // create a hard coded array
+        int[] hardCodedArray = {10, 20, 10, 20, 60, 10, 5, 60, 5, 60};
+        // generate a random array
+        int[] randomArray = DataGenerator.getRandomArray(10);
+
+        // display arrays
+        System.out.println("Hard coded array contents: ");
+        System.out.println(Arrays.toString(hardCodedArray));
+
+        System.out.println("\nRandomly generated array contents: ");
+        if (randomArray != null) {
+            System.out.println(Arrays.toString(randomArray));
+        } else {
+            System.out.println("Random array cannot be generated");
+
+        }
+    }
 }
+

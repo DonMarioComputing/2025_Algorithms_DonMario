cf4af838357e1995bd53cc1f1804f70568486ba9
DonMarioComputing <omeshmariodon@gmail.com>
2025-11-05 21:24:53 +0000

    "feat: Generate random array of Books and display the contents of the array"

--- src/main/java/ui/Library.java
+++ src/main/java/ui/Library.java
@@ -1,4 +1,25 @@
 package ui;
 
+import entities.Book;
+import utils.BookGenerator;
+import utils.BookUtils;
+
+import java.util.Arrays;
+import java.util.Random;
+import java.util.Scanner;
+
 public class Library {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+
+        // generate random array of Books
+        //1.Display the contents of the array
+        Book[] books = BookGenerator.getRandomArray(10);
+
+        System.out.println("Initial array of books:");
+        System.out.println(BookUtils.toString(books));
+
+    }
 }

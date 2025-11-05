a0d9801ec4f711e5d3d8e25d75f7e59ba1a67f3e
DonMarioComputing <omeshmariodon@gmail.com>
2025-11-05 21:32:38 +0000

    "feat: Delete a random Book from the array based on position"

--- src/main/java/ui/Library.java
+++ src/main/java/ui/Library.java
@@ -24,5 +24,10 @@ public class Library {
         //2.sort the array first [to avoid null issues [as the amendment email]]
         Arrays.sort(books);
 
+        //3.Delete a random Book from the array based on position (remember to inform the user what was deleted)
+        int randomIndex = random.nextInt(books.length);
+        Book removedBook = BookUtils.emptyIndex(books, randomIndex);
+        System.out.println("\nRandomly removed book at position " + randomIndex + ": " + removedBook);
+
     }
 }

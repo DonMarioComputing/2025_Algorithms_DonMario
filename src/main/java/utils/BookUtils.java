8c508229eb833c0b0674dfc2bd79e7666cc6a9df
DonMarioComputing <omeshmariodon@gmail.com>
2025-11-05 19:01:02 +0000

    "feat: implement emptyIndex() method to overwrite a book at a given index" "docs: add JavaDoc for emptyIndex() method"

--- src/main/java/utils/BookUtils.java
+++ src/main/java/utils/BookUtils.java
@@ -39,7 +39,38 @@ public class BookUtils {
 
             return result;
         }
+
+    //Exercise 2:
+    /**
+     * Removes the Book at a specified index in the supplied array using an overwriting
+     * @param books the array of objects to modify
+     * @param index the position in the array to overwrite
+     * @return the object that was previously stored at that position or if that slot was already empty
+     * @throws IllegalArgumentException if the supplied array is null
+     * @throws IndexOutOfBoundsException if the supplied index is less than 0 or greater than or equal to the array length
+     */
+     public static Book emptyIndex(Book[] books, int index) {
+         // check for a null array
+         if (books == null) {
+            throw new IllegalArgumentException("Book array cannot be null");
+         }
+
+         // check for invalids, less than 0 or outside array bounds
+         if (index < 0 || index >= books.length) {
+            throw new IndexOutOfBoundsException("Index " + index + " is out of bounds");
+         }
+
+         // store the book currently at the given index
+         Book removedBook = books[index];
+
+         // overwrite that index with null
+         books[index] = null;
+
+         // return the original book that was stored at this position
+         return removedBook;
     }
+
+}
 
 
 
